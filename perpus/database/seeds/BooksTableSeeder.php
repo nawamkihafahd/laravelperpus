ebffdeb83e2fa0c0f0dcfb2c73bd710a28e7ca5b
Dhafa Hikmawan <32931992+nawamkihafahd@users.noreply.github.com>
2019-06-20 16:07:47 +0700

    seed and migration

---
+++ perpus/database/seeds/BooksTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+		DB::table('books')->insert([
+            'judul' => 'kkn',
+            'pengarang' => 'aghaw',
+			'coverurl' => 'coverurls/V9jXhiUMF3Lt3hMyB2yQX5ICRt8YKVIL2nkceSbZ.png',
+            'fileurl' => 'fileurls/XBZvMIUt4xgWzXCritwIISIqNkEWORJ13OsVeB1M.pdf'),
+        ]);
+    }
+}
